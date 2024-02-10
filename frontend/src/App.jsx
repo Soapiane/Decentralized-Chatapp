faca75ec1b4bc0f502ffa9d1a4790f9d22eed3b2
Soapiane <Ls_yekene@esi.dz>
2024-02-10 15:17:09 +0100

    Initialized base technologies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="w-full flex align-middle justify-center">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
